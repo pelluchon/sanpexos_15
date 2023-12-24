3937db3057efef7ad8fee5a6942094d8e7c4fcb8
pelluchon <85738940+pelluchon@users.noreply.github.com>
2023-12-24 11:28:53 +0100

    Update start.py

--- start.py
+++ start.py
@@ -823,7 +823,7 @@ def open_trade(df, fx, tick, trading_settings_provider, dj):
             and df.iloc[-2]['AskClose'] > df.iloc[index_peak:-2]['AskClose'].mean() \
             and df.iloc[-2]['AskHigh'] > df.iloc[-2]['tenkan_avg'] \
             and df.iloc[-2]['AskClose'] > df.iloc[-2]['kijun_avg'] \
-            and df.iloc[-2]['AskLow'] > max(df.iloc[index_peak]['senkou_a'], df.iloc[index_peak]['senkou_b']) \
+            and df.iloc[-2]['AskLow'] > max(df.iloc[-2]['senkou_a'], df.iloc[-2]['senkou_b']) \
             and df.iloc[-2]['tenkan_avg'] > df.iloc[-2]['kijun_avg'] \
             and df.iloc[index_peak]['kijun_avg'] < min(df.iloc[index_peak]['senkou_a'], df.iloc[index_peak]['senkou_b']) \
             and df.iloc[index_peak]['tenkan_avg'] < df.iloc[index_peak]['kijun_avg'] \
@@ -854,7 +854,7 @@ def open_trade(df, fx, tick, trading_settings_provider, dj):
             and df.iloc[-2]['AskClose'] < df.iloc[index_peak:-2]['AskClose'].mean() \
             and df.iloc[-2]['AskLow'] < df.iloc[-2]['tenkan_avg'] \
             and df.iloc[-2]['AskClose'] < df.iloc[-2]['kijun_avg'] \
-            and df.iloc[-2]['AskHigh'] < min(df.iloc[index_peak]['senkou_a'], df.iloc[index_peak]['senkou_b']) \
+            and df.iloc[-2]['AskHigh'] < min(df.iloc[-2]['senkou_a'], df.iloc[-2]['senkou_b']) \
             and df.iloc[-2]['tenkan_avg'] < df.iloc[-2]['kijun_avg'] \
             and ((df.iloc[-2]['slope_macd'] < 0) or (df.iloc[-2]['macd'] < df.iloc[index_peak]['macd'])) \
             and df.iloc[index_peak]['kijun_avg'] > max(df.iloc[index_peak]['senkou_a'], df.iloc[index_peak]['senkou_b']) \
