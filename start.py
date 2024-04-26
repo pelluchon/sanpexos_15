ba2002f0464d9f57eb5032e0a3173328d9a44de9
pelluchon <85738940+pelluchon@users.noreply.github.com>
2024-04-26 11:05:55 +0200

    Add files via upload

--- start.py
+++ start.py
@@ -381,7 +381,7 @@ def close_trade(df, fx, tick, dj, idx):
                         pass
                 else:
                     try:
-                        sl = df.iloc[open_rev_index:idx]['AskLow'].min()
+                        sl = df.iloc[open_rev_index:idx]['AskLow'][df.iloc[open_rev_index:idx]['AskLow']>min(df.iloc[open_rev_index:idx]['senkou_a'].min(),df.iloc[open_rev_index:idx]['senkou_b'].min())].min()
                         request = fx.create_order_request(
                             order_type=fxcorepy.Constants.Orders.STOP,
                             command=fxcorepy.Constants.Commands.CREATE_ORDER,
@@ -444,7 +444,7 @@ def close_trade(df, fx, tick, dj, idx):
                         pass
                 else:
                     try:
-                        sl = df.iloc[open_rev_index:idx]['AskHigh'].max()
+                        sl = df.iloc[open_rev_index:idx]['AskHigh'][df.iloc[open_rev_index:idx]['AskHigh']>max(df.iloc[open_rev_index:idx]['senkou_a'].max(),df.iloc[open_rev_index:idx]['senkou_b'].max())].max()
                         request = fx.create_order_request(
                             order_type=fxcorepy.Constants.Orders.STOP,
                             command=fxcorepy.Constants.Commands.CREATE_ORDER,
