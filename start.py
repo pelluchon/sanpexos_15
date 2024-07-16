6df3c3f00cd0f3b2a5304d9928527267fef232b0
pelluchon <85738940+pelluchon@users.noreply.github.com>
2024-07-16 22:17:54 +0200

    Update start.py

--- start.py
+++ start.py
@@ -197,9 +197,9 @@ def should_close_buy_trade(df,idx,idx_open,dj):
         abs(df.iloc[idx]['BidClose'] - df.iloc[idx]['BidOpen'])>abs(df.iloc[idx-7:idx]['BidHigh'].max() - df.iloc[idx-7:idx]['BidLow'].min()) and\
         df.iloc[idx]['BidClose'] < df.iloc[idx-3:idx]['tenkan_avg'].mean():
         result = 'Kill for high tendance change'
-    elif df.iloc[idx]['BidOpen'] > df.iloc[idx]['Bollinger_2'] and candle_m2<0.5 and \
+    elif df.iloc[idx]['BidClose'] > df.iloc[idx]['Bollinger_2'] and candle_m2<0.25 and \
         df.iloc[idx-1:idx]['rsi'].mean() > 70:
-        result ='Kill for Bollinger'
+        result ='Kill for Bollinger 2'
     elif df.iloc[idx]['BidClose'] < df.iloc[idx]['Bollinger_0'] and \
         df.iloc[idx]['tenkan_avg'] < df.iloc[idx]['Bollinger_0'] and \
         df.iloc[idx]['signal'] > df.iloc[idx]['macd']:
@@ -254,9 +254,9 @@ def should_close_sell_trade(df,idx,idx_open,dj):
         df.iloc[idx]['tenkan_avg'] > df.iloc[idx]['Bollinger_0'] and \
         df.iloc[idx]['signal'] < df.iloc[idx]['macd']:
         result ='Kill for Bollinger 0'
-    elif df.iloc[idx]['BidOpen'] < df.iloc[idx]['Bollinger_0'] and candle_m2>-0.5 and \
+    elif df.iloc[idx]['BidClose'] < df.iloc[idx]['Bollinger_-2'] and candle_m2>-0.25 and \
         df.iloc[idx-1:idx]['rsi'].mean()< 30:
-        result ='Kill for Bollinger'
+        result ='Kill for Bollinger -2'
     elif df.iloc[idx]['BidClose'] < df.iloc[idx_open]['BidClose'] :
         result = 'Save minimum'
     else:
